3dfd1c9cde75b7c27e060b4974b86fe563385604
Bruno Prada <brunohprada@hotmail.com>
2022-04-02 11:46:35 -0300

    params no gin

--- controllers/controller.go
+++ controllers/controller.go
@@ -1,10 +1,19 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func ExibeTodosAlunos(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"id":   "1",
 		"Nome": "Bruno",
+	})
+}
+
+func Saudacao(c *gin.Context) {
+	nome := c.Params.ByName("nome")
+	c.JSON(200, gin.H{
+		"API diz:": "E ai " + nome + ", tudo beleza?",
 	})
 }
