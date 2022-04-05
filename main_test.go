943fd1d2beda7c19a1b92f8cf72d82a7d3c526b5
Bruno Prada <brunohprada@hotmail.com>
2022-04-05 07:39:31 -0300

    assert

--- main_test.go
+++ main_test.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
+	"github.com/brunohprada/go-api-gin/controllers"
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
 )
 
 func SetupDasRotasDeTeste() *gin.Engine {
@@ -11,6 +17,16 @@ func SetupDasRotasDeTeste() *gin.Engine {
 	return rotas
 }
 
-func TestFalhador(t *testing.T) {
-	t.Fatalf("Teste falhou de propóstio, não se preocupe")
+func TestVerificaStatusCodeDaSaudacaoComParametro(t *testing.T) {
+	r := SetupDasRotasDeTeste()
+	r.GET("/:nome", controllers.Saudacao)
+	req, _ := http.NewRequest("GET", "/bruno", nil)
+	resposta := httptest.NewRecorder()
+	r.ServeHTTP(resposta, req)
+	assert.Equal(t, http.StatusOK, resposta.Code, "Deveriam ser iguais")
+	mockDaResposta := `{"API diz:":"E ai bruno, tudo beleza?"}`
+	respostaBody, _ := ioutil.ReadAll(resposta.Body)
+	assert.Equal(t, mockDaResposta, string(respostaBody))
+	fmt.Println(string(respostaBody))
+	fmt.Println(string(mockDaResposta))
 }
